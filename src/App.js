d5c652137f026cdcda5de1e7c6208bea1ae08fce
Doug <doug@tech-center.com>
2019-04-23 18:48:46 -0400

    Added completed window and more State and styling

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ function App() {
     }
   ]);
 
+  const [completed, setCompletedState] = useState([]);
+
+  const [isHidden, setHidden] = useState(true);
+
   function Todo({ todo, index, completeTodo, removeTodo }) {
     return (
       <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
@@ -28,6 +32,17 @@ function App() {
         <div>
           <button className="todoButton" onClick={() => completeTodo(index)}>Completed</button>
           <button className="todoButton" onClick={() => removeTodo(index)}>X</button>
+        </div>
+      </div>
+    );
+  }
+
+  function CompletedTodo({ todo, index, completeTodo, removeCompletedTodo }) {
+    return (
+      <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+        {todo.toDoString}
+        <div>
+          <button className="todoButton" onClick={() => removeCompletedTodo(index)}>X</button>
         </div>
       </div>
     );
@@ -61,20 +76,47 @@ function App() {
     setTodosState(todosArray);
   };
 
+  const removeCompetedTodo = index => {
+    const completedTodosArray = [...completed];
+    completedTodosArray.splice(index, 1);
+    setCompletedState(completedTodosArray);
+  };
+
   const completeTodo = index => {
     const todosArray = [...todos];
+    const thisString = todosArray[index].toDoString;
+    console.log('thisString: ' + thisString);
     todosArray[index].isCompleted = true;
+    const completedArray = [...completed, todosArray[index]];
+    setCompletedState(completedArray);
+    todosArray.splice(index, 1);
     setTodosState(todosArray);
   };
 
+  const toggleHidden = () => {
+    if (isHidden === true) {
+      setHidden(false);
+    } else {
+      setHidden(true);
+    }
+  }
+
   return (
     <div className="app">
-      <div className="todo-list">
+      <div className="clickButton noselect" onClick={toggleHidden}>Todos</div>
+      {isHidden ? null : <div className="todo-list">
+        <h3 className="label noselect">ToDo List</h3>
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo} />
+          <Todo className="todo" key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo} />
         ))}
         <Todos addTodo={addTodo} />
-      </div>
+      </div>}
+      {isHidden ? null : <div className="completed-list">
+        <h3 className="label noselect">Completed</h3>
+        {completed.map((todo, index) => (
+          <CompletedTodo className="completedTodo" key={index} index={index} todo={todo} completeTodo={completeTodo} removeCompletedTodo={removeCompetedTodo} />
+        ))}
+      </div>}
     </div>
   );
 }
